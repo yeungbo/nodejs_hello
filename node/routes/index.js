64c1c5814b2eea717d7c9f8653ea87e1e3a8545f
yangbo <yangbo@9.83.30.3>
2018-06-05 22:59:30 +0800

    update

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express111' });
+  res.render('index', { title: 'Hello NodeJS' });
 });
 
 module.exports = router;
