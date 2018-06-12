0c1ac0eb0c6098cad98b04d1cb2a52ab658db2dc
yangbo <yangbo@windows10.microdone.cn>
2018-06-12 19:04:11 +0800

    test

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ var PORT = 8888;
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world - This is Bllue Version\n');
+  res.send('Hello world - This is Bllue0 Version\n');
 });
 
 app.listen(PORT);
