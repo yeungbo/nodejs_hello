eea4f18c96c9069a3e03551a6fc883aec8887e6d
yangbo <yangbbo@cn.ibm.com>
2018-06-13 00:11:47 +0800

    newupdate

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ var PORT = 8888;
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World - This is Green Version\n');
+  res.send('Hello World - This is Green00 Version\n');
 });
 
 app.listen(PORT);
