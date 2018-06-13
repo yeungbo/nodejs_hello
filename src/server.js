6e9d69ca5786f18ffbeb0c2cbcb7b67f4ede0648
yangbo <yangbo@windows10.microdone.cn>
2018-06-13 23:04:28 +0800

    1

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ var PORT = 8888;
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World - This is green one test Version\n');
+  res.send('Hello World - This is green 1 Version\n');
 });
 
 app.listen(PORT);
