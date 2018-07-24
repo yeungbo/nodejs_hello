a92dc1c1c681e3a1dfbe4e48325bec998221fe3b
yangbo <yangbo@windows10.microdone.cn>
2018-07-24 22:39:20 +0800

    3

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ var PORT = 61001;
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World SDT - This is fake app Version beta 1\n');
+  res.send('Hello World SDT - This is fake app Version beta 3\n');
 });
 
 app.listen(PORT);
