9417f8925a1dc50e0748a518c413b068f37556e1
yangbo <yangbo@windows10.microdone.cn>
2018-07-25 22:16:35 +0800

    hello guys

--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@
 
 var express = require('express');
 
-var PORT = 61112;
+var PORT = 61105;
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World SDT - This is demo-one app Version beta 01\n');
+  res.send('Hello World SDT - This is hello-guys app Version 01\n');
 });
 
 app.listen(PORT);
