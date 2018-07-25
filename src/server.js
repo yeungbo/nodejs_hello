c83666f01dbe32163df37b72cb230034257c1fe7
yangbo <yangbo@windows10.microdone.cn>
2018-07-25 21:40:58 +0800

    demo-one

--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@
 
 var express = require('express');
 
-var PORT = 61111;
+var PORT = 61112;
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World SDT - This is fake-service01 app Version beta 02\n');
+  res.send('Hello World SDT - This is demo-one app Version beta 01\n');
 });
 
 app.listen(PORT);
