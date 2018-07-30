1a78cfb48590a3ba782a3dbd114b5e62b3e22a9f
yangbo <yangbo@windows10.microdone.cn>
2018-07-30 19:46:50 +0800

    auto05

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ var PORT = 61105;
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World SDT - This is auto04 service - Version beta0a\n');
+  res.send('Hello World SDT - This is auto05 service - Version beta0a\n');
 });
 
 app.listen(PORT);
