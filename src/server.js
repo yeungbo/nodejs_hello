cb15c1e6f720e4b14ebbeaf085038f7022e13535
yangbo <yangbo@windows10.microdone.cn>
2018-07-30 21:42:41 +0800

    demoshow01 app

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ var PORT = 61105;
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World SDT - This is auto06 service - Version beta0a\n');
+  res.send('Hello World SDT - This is demoshow01 service - Version alpha01\n');
 });
 
 app.listen(PORT);
