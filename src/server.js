505657beb4f2ebd29d58086fbbe70cce1b9ca22e
yangbo <yangbo@windows10.microdone.cn>
2018-08-01 22:40:48 +0800

    nodejs service v1

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ var PORT = 61105;
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World SDT - This is for demo2q03 service - Version v3\n');
+  res.send('Hello World SDT - This is for nodejsservice service - Version v1\n');
 });
 
 app.listen(PORT);
